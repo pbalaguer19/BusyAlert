4f986b561e87ed3421f49d2e53071cfc383a8321
Pau <pbalaguer19@gmail.com>
2017-02-28 09:35:41 +0100

    ToolBar done

--- app/src/main/java/com/example/pau/busyalert/MainActivity.java
+++ app/src/main/java/com/example/pau/busyalert/MainActivity.java
@@ -1,20 +1,62 @@
 package com.example.pau.busyalert;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    /** TOOLBAR VARIABLES **/
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+
+    /** OPTIONS VARIABLES **/
+    SeekBar seekBar;
+    TextView speed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new HomeFragment(), "Home");
+        viewPagerAdapter.addFragments(new SocialFragment(), "Social");
+        viewPagerAdapter.addFragments(new OptionsFragment(), "Options");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        /*seekBar = (SeekBar) findViewById(R.id.seekBar);
+        speed = (TextView) findViewById(R.id.speed);
+        speed.setText("15");
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                speed.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });*/
     }
 }
---
+++ app/src/main/java/com/example/pau/busyalert/ViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.example.pau.busyalert;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by pau on 27/2/17.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> tabTitles = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    public void addFragments(Fragment fragments, String tabTitles){
+        this.fragments.add(fragments);
+        this.tabTitles.add(tabTitles);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles.get(position);
+    }
+}
