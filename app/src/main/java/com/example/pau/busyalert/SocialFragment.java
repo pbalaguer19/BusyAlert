c22cd361c665f482aa1aea4ebd62e538347b1bad
Pau <pbalaguer19@gmail.com>
2017-03-01 13:24:11 +0100

    Starting Social Listview.

--- app/src/main/java/com/example/pau/busyalert/SocialFragment.java
+++ app/src/main/java/com/example/pau/busyalert/SocialFragment.java
@@ -1,29 +1,50 @@
 package com.example.pau.busyalert;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.provider.ContactsContract.Contacts;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class SocialFragment extends Fragment {
 
-
-    public SocialFragment() {
-        // Required empty public constructor
-    }
-
+public class SocialFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private SimpleCursorAdapter mAdapter;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_social, container, false);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setEmptyText("No phone numbers");
+
+        mAdapter = new SimpleCursorAdapter(getActivity(),
+                android.R.layout.simple_list_item_2, null, new String[]{
+                Contacts.DISPLAY_NAME, Contacts.CONTACT_STATUS},
+                new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        setListAdapter(mAdapter);
+
+        getLoaderManager().initLoader(0, null, this);
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {Contacts._ID, Contacts.DISPLAY_NAME,
+                Contacts.CONTACT_STATUS};
+        return new CursorLoader(getActivity(), Contacts.CONTENT_URI,
+                projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
 }
+
