30de5af3456efb9fd382d15d5b285b228fc8724c
Pau <pbalaguer19@gmail.com>
2017-03-01 12:32:10 +0100

    Welcome page done. Settings not working.

--- app/src/main/java/com/example/pau/busyalert/MainActivity.java
+++ app/src/main/java/com/example/pau/busyalert/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.pau.busyalert;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     /** TOOLBAR VARIABLES **/
@@ -30,8 +30,16 @@ public class MainActivity extends AppCompatActivity {
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPagerAdapter.addFragments(new HomeFragment(), "Home");
         viewPagerAdapter.addFragments(new SocialFragment(), "Social");
-        viewPagerAdapter.addFragments(new OptionsFragment(), "Options");
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.settings_item){
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/pau/busyalert/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.pau.busyalert;
+
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
