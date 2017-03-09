a6f136f65bc4f3ae45991e8f1046cab0154de74f
Pau <pbalaguer19@gmail.com>
2017-03-09 12:55:06 +0100

    IntentService for activity recognition working

--- app/src/main/java/com/example/pau/busyalert/ActivityRecognitionService.java
+++ app/src/main/java/com/example/pau/busyalert/ActivityRecognitionService.java
@@ -1,6 +1,7 @@
 package com.example.pau.busyalert;
 
 import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -10,11 +11,8 @@ import com.google.android.gms.location.DetectedActivity;
 
 import java.util.List;
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- */
 public class ActivityRecognitionService extends IntentService {
+
     public ActivityRecognitionService() {
         super("ActivityRecognitionService");
     }
@@ -23,6 +21,7 @@ public class ActivityRecognitionService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if (intent != null && ActivityRecognitionResult.hasResult(intent)) {
             ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+            DetectedActivity detectedActivity = result.getMostProbableActivity();
             handleDetectedActivity(result.getProbableActivities());
         }
     }
@@ -31,12 +30,6 @@ public class ActivityRecognitionService extends IntentService {
         for(DetectedActivity detectedActivity: probableActivities){
             if(detectedActivity.getType() == DetectedActivity.IN_VEHICLE &&
                     detectedActivity.getConfidence() > 70){
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-                builder.setContentText(getString(R.string.driving));
-                builder.setSmallIcon(R.mipmap.ic_launcher);
-                builder.setContentTitle(getString(R.string.app_name));
-                NotificationManagerCompat.from(this).notify(0, builder.build());
-            }else{
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
                 builder.setContentText(getString(R.string.driving));
                 builder.setSmallIcon(R.mipmap.ic_launcher);
--- app/src/main/java/com/example/pau/busyalert/LocatBroadcastReciver.java
+++ app/src/main/java/com/example/pau/busyalert/LocatBroadcastReciver.java
@@ -4,13 +4,21 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
 import com.google.android.gms.location.LocationResult;
+
+import java.util.List;
 
 /**
  * Created by pau on 6/3/17.
  */
 public class LocatBroadcastReciver extends BroadcastReceiver{
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if(LocationResult.hasResult(intent)){
@@ -22,4 +30,5 @@ public class LocatBroadcastReciver extends BroadcastReceiver{
             Toast.makeText(context, text, Toast.LENGTH_LONG).show();
         }
     }
+
 }
